f0d01a8a5881709310ca077da9e7b640b4fdee51
Zoc Nishant <zocnishant@gmail.com>
2024-07-29 15:25:57 +0545

    loader almost done

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-gsap.from(".line h1", {
+let time = gsap.timeline();
+
+// loader animation at the first coming from down
+time.from(".line h1", {
   y: 150,
   stagger: 0.25,
   duration: 0.6,
   delay: 0.5,
+});
+
+// for text now
+
+time.from("line1-part1, .line h2", {
+  opacity: 0,
+  onStart: function () {
+    // counter 0 - 100
+    let h5timer = document.querySelector("#line1-part1 h5");
+    let counter = 0;
+
+    setInterval(function () {
+      if (counter < 100) {
+        h5timer.innerHTML = counter++;
+      } else {
+        h5timer.innerHTML = counter;
+      }
+    }, 30);
+  },
+});
+
+time.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 4,
+});
+
+time.from("#page1", {
+  delay: 0.2,
+  y: 1600,
+  opacity: 0,
+  ease: Power4,
+  duration: 0.5,
+});
+
+time.to("#loader", {
+  display: none,
 });
